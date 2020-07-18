af6b7c5d909e7316bce7f53c4426607ca8f8cfba
itor isaias da silva <itor.isaias@gmail.com>
2020-07-18 18:20:49 -0300

    setup relay

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { RelayEnvironmentProvider } from 'relay-hooks';
 
 import Template from 'Template'
 import { GlobalStyle } from 'Styles'
 import Themes from 'Themes'
 
+import RelayEnvironment from "Relay";
+
 function App() {
   return (
     <ThemeProvider theme={Themes}>
       <GlobalStyle />
-      <Template />
+      <RelayEnvironmentProvider environment={RelayEnvironment} >
+        <Template />
+      </RelayEnvironmentProvider>
     </ThemeProvider>
   );
 }
